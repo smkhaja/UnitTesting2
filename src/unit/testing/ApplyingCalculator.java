9608f3e91e716dcef9cb16963363fa69e424c1a5
smkhaja <smkhaja2000@gmail.com>
2018-04-07 14:47:28 -0400

    first commit

---
+++ src/unit/testing/ApplyingCalculator.java
@@ -0,0 +1,11 @@
+package unit.testing;
+
+
+public class ApplyingCalculator {
+	public static void main(String[]agrs) {
+		Calculator cal = new Calculator();
+		Assert.
+		
+	}
+
+}
---
+++ src/unit/testing/Calculator.java
@@ -0,0 +1,14 @@
+package unit.testing;
+
+public class Calculator {
+	String name;
+	public int addition (int num1, int num2) {
+		int total = num1 + num2;
+		return total;
+	}
+	public int subtruction (int num1, int num2) {
+		int total = num1 - num2;
+		return total;
+	}
+
+}
